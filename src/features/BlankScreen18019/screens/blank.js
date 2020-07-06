a8e869287e27d9350b77ad474044569f15e350c0
Crowdbotics <team@crowdbotics.com>
2020-07-06 13:55:00 +0000

    Update components

--- src/features/BlankScreen18019/screens/blank.js
+++ src/features/BlankScreen18019/screens/blank.js
@@ -18,7 +18,13 @@ export class _Blank extends React.Component {
     title: "Blank".toUpperCase()
   }
 
-  state = { Datepicker_12: new Date("07/06/2020") }
+  state = {
+    Datepicker_12: new Date("07/06/2020"),
+    Toggle_16: true,
+    Radio_58: true,
+    CheckBox_60: true,
+    CheckBox_22: true
+  }
 
   render = () => (
     <View
@@ -269,6 +275,71 @@ export class _Blank extends React.Component {
                   fontFamily: "System"
                 }}
               />
+              <Toggle
+                activeColor="#409EFF"
+                inactiveColor="#C0CCDA"
+                disabled={false}
+                text="switch ON/OFF"
+                style={{
+                  marginLeft: 0,
+                  marginRight: 0,
+                  marginTop: 5,
+                  marginBottom: 5,
+                  paddingLeft: 5,
+                  paddingRight: 5,
+                  paddingTop: 5,
+                  paddingBottom: 5,
+                  overflow: "visible",
+                  textAlign: "left",
+                  verticalAlign: "baseline",
+                  borderColor: "#000000",
+                  borderStyle: "solid",
+                  borderWidth: 0,
+                  borderLeftWidth: 0,
+                  borderRightWidth: 0,
+                  borderTopWidth: 0,
+                  borderBottomWidth: 0,
+                  borderRadius: 0,
+                  fontFamily: "System"
+                }}
+                checked={this.state.Toggle_16}
+                onChange={nextChecked =>
+                  this.setState({ Toggle_16: nextChecked })
+                }
+              />
+              <Icon
+                iconFont="Eva Design Icons"
+                name="github"
+                width={20}
+                height={20}
+                style={{
+                  marginLeft: 0,
+                  marginRight: 0,
+                  marginTop: 5,
+                  marginBottom: 5,
+                  paddingLeft: 5,
+                  paddingRight: 5,
+                  paddingTop: 5,
+                  paddingBottom: 5,
+                  overflow: "visible",
+                  textAlign: "left",
+                  verticalAlign: "baseline",
+                  borderColor: "#000000",
+                  borderStyle: "solid",
+                  borderWidth: 0,
+                  borderLeftWidth: 0,
+                  borderRightWidth: 0,
+                  borderTopWidth: 0,
+                  borderBottomWidth: 0,
+                  borderRadius: 0,
+                  fontSize: 20,
+                  color: "#000000",
+                  backgroundColor: "#ffffff",
+                  fontStyle: "normal",
+                  fontWeight: "normal",
+                  fontFamily: "System"
+                }}
+              />
             </View>
             <View
               style={{
@@ -282,7 +353,7 @@ export class _Blank extends React.Component {
                 paddingRight: 0,
                 paddingTop: 0,
                 paddingBottom: 0,
-                overflow: "visible",
+                overflow: "scroll",
                 textAlign: "left",
                 verticalAlign: "baseline",
                 fontSize: 20,
@@ -306,7 +377,38 @@ export class _Blank extends React.Component {
                 borderRadius: 0,
                 backgroundSize: "auto"
               }}
-            />
+            >
+              <Radio
+                text="Radio button"
+                disabled={false}
+                style={{
+                  marginLeft: 0,
+                  marginRight: 0,
+                  marginTop: 5,
+                  marginBottom: 5,
+                  paddingLeft: 5,
+                  paddingRight: 5,
+                  paddingTop: 5,
+                  paddingBottom: 5,
+                  overflow: "visible",
+                  textAlign: "left",
+                  verticalAlign: "baseline",
+                  borderColor: "#000000",
+                  borderStyle: "solid",
+                  borderWidth: 0,
+                  borderLeftWidth: 0,
+                  borderRightWidth: 0,
+                  borderTopWidth: 0,
+                  borderBottomWidth: 0,
+                  borderRadius: 0,
+                  fontFamily: "System"
+                }}
+                checked={this.state.Radio_58}
+                onChange={nextChecked =>
+                  this.setState({ Radio_58: nextChecked })
+                }
+              />
+            </View>
           </View>
         </View>
         <View
@@ -321,7 +423,7 @@ export class _Blank extends React.Component {
             paddingRight: 0,
             paddingTop: 0,
             paddingBottom: 0,
-            overflow: "visible",
+            overflow: "scroll",
             textAlign: "left",
             verticalAlign: "baseline",
             fontSize: 20,
@@ -345,8 +447,137 @@ export class _Blank extends React.Component {
             borderRadius: 0,
             backgroundSize: "auto"
           }}
-        />
+        >
+          <CheckBox
+            text="Checkbox"
+            disabled={false}
+            style={{
+              marginLeft: 0,
+              marginRight: 0,
+              marginTop: 5,
+              marginBottom: 5,
+              paddingLeft: 5,
+              paddingRight: 5,
+              paddingTop: 5,
+              paddingBottom: 5,
+              overflow: "visible",
+              textAlign: "left",
+              verticalAlign: "baseline",
+              borderColor: "#000000",
+              borderStyle: "solid",
+              borderWidth: 0,
+              borderLeftWidth: 0,
+              borderRightWidth: 0,
+              borderTopWidth: 0,
+              borderBottomWidth: 0,
+              borderRadius: 0,
+              fontFamily: "System"
+            }}
+            checked={this.state.CheckBox_60}
+            onChange={nextChecked =>
+              this.setState({ CheckBox_60: nextChecked })
+            }
+          />
+          <View
+            style={{
+              width: "100%",
+              height: "100%",
+              marginLeft: 0,
+              marginRight: 0,
+              marginTop: 0,
+              marginBottom: 0,
+              paddingLeft: 0,
+              paddingRight: 0,
+              paddingTop: 0,
+              paddingBottom: 0,
+              overflow: "visible",
+              textAlign: "left",
+              verticalAlign: "baseline",
+              fontSize: 20,
+              color: "#000000",
+              backgroundColor: "#ffffff",
+              fontStyle: "normal",
+              fontWeight: "normal",
+              fontFamily: "System",
+              flexDirection: "column",
+              flexWrap: "wrap",
+              justifyContent: "flex-start",
+              alignItems: "stretch",
+              alignContent: "stretch",
+              borderColor: "#000000",
+              borderStyle: "solid",
+              borderWidth: 0,
+              borderLeftWidth: 0,
+              borderRightWidth: 0,
+              borderTopWidth: 0,
+              borderBottomWidth: 0,
+              borderRadius: 0,
+              backgroundSize: "auto"
+            }}
+          />
+        </View>
       </View>
+      <Icon
+        iconFont="Eva Design Icons"
+        name="github"
+        width={20}
+        height={20}
+        style={{
+          marginLeft: 0,
+          marginRight: 0,
+          marginTop: 5,
+          marginBottom: 5,
+          paddingLeft: 5,
+          paddingRight: 5,
+          paddingTop: 5,
+          paddingBottom: 5,
+          overflow: "visible",
+          textAlign: "left",
+          verticalAlign: "baseline",
+          borderColor: "#000000",
+          borderStyle: "solid",
+          borderWidth: 0,
+          borderLeftWidth: 0,
+          borderRightWidth: 0,
+          borderTopWidth: 0,
+          borderBottomWidth: 0,
+          borderRadius: 0,
+          fontSize: 20,
+          color: "#000000",
+          backgroundColor: "#ffffff",
+          fontStyle: "normal",
+          fontWeight: "normal",
+          fontFamily: "System"
+        }}
+      />
+      <CheckBox
+        text="Checkbox"
+        disabled={false}
+        style={{
+          marginLeft: 0,
+          marginRight: 0,
+          marginTop: 5,
+          marginBottom: 5,
+          paddingLeft: 5,
+          paddingRight: 5,
+          paddingTop: 5,
+          paddingBottom: 5,
+          overflow: "visible",
+          textAlign: "left",
+          verticalAlign: "baseline",
+          borderColor: "#000000",
+          borderStyle: "solid",
+          borderWidth: 0,
+          borderLeftWidth: 0,
+          borderRightWidth: 0,
+          borderTopWidth: 0,
+          borderBottomWidth: 0,
+          borderRadius: 0,
+          fontFamily: "System"
+        }}
+        checked={this.state.CheckBox_22}
+        onChange={nextChecked => this.setState({ CheckBox_22: nextChecked })}
+      />
     </View>
   )
 }
