f98aea7080e607b6aa36d155856d562c30eba384
Crowdbotics <team@crowdbotics.com>
2020-07-06 07:39:12 +0000

    Update models

--- backend/home/migrations/0003_auto_20200706_0728.py
+++ backend/home/migrations/0003_auto_20200706_0728.py
@@ -6,23 +6,23 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('home', '0002_load_initial_data'),
+        ("home", "0002_load_initial_data"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='customtext',
-            name='jhgjhgf',
+            model_name="customtext",
+            name="jhgjhgf",
             field=models.IntegerField(blank=True, null=True),
         ),
         migrations.AddField(
-            model_name='customtext',
-            name='jhgjhtjg',
+            model_name="customtext",
+            name="jhgjhtjg",
             field=models.DurationField(blank=True, null=True),
         ),
         migrations.AddField(
-            model_name='customtext',
-            name='kjyuiuyiuy',
+            model_name="customtext",
+            name="kjyuiuyiuy",
             field=models.BigIntegerField(blank=True, null=True),
         ),
     ]
--- backend/home/models.py
+++ backend/home/models.py
@@ -8,9 +8,6 @@ from django.db import models
 
 class CustomText(models.Model):
     title = models.CharField(max_length=150,)
-    jhgjhtjg = models.DurationField(null=True, blank=True,)
-    kjyuiuyiuy = models.BigIntegerField(null=True, blank=True,)
-    jhgjhgf = models.IntegerField(null=True, blank=True,)
 
     def __str__(self):
         return self.title
